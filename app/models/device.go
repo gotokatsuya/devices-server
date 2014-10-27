472c388f04c7938fcd5f6e46460236a4caea8244
goto <katsuya.goto@eure.jp>
2014-10-27 23:41:59 +0900

    Modify name

--- app/models/device.go
+++ app/models/device.go
@@ -8,21 +8,21 @@ import (
  * 端末モデル
  */
 type Device struct {
-	Id            int64          `json:"id,omitempty"`
-	Name          string         `json:"name,omitempty"`
-	Manufacturer  string         `json:"manufacturer,omitempty"`
-	Carrier       string         `json:"carrier,omitempty"`
-	Os            string         `json:"os,omitempty"`
-	Size          string         `json:"size,omitempty"`
-	Resolution    string         `json:"resolution,omitempty"`
-	Memory        string         `json:"memory,omitempty"`
-	DateOfRelease int64          `json:"dataOfRelease,omitempty"`
-	Other         string         `json:"other,omitempty"`
-	CreatedAt     int64          `json:"createdAt,omitempty"`
-	UpdatedAt     int64          `json:"updatedAt,omitempty"`
-	DeletedAt     int64          `json:"deletedAt,omitempty"`
-	User          User           `json:"user,omitempty"`
-	DeviceStates  []DeveiceState `json:"deviceStates,omitempty"`
+	Id            int64         `json:"id,omitempty"`
+	Name          string        `json:"name,omitempty"`
+	Manufacturer  string        `json:"manufacturer,omitempty"`
+	Carrier       string        `json:"carrier,omitempty"`
+	Os            string        `json:"os,omitempty"`
+	Size          string        `json:"size,omitempty"`
+	Resolution    string        `json:"resolution,omitempty"`
+	Memory        string        `json:"memory,omitempty"`
+	DateOfRelease int64         `json:"dataOfRelease,omitempty"`
+	Other         string        `json:"other,omitempty"`
+	CreatedAt     int64         `json:"createdAt,omitempty"`
+	UpdatedAt     int64         `json:"updatedAt,omitempty"`
+	DeletedAt     int64         `json:"deletedAt,omitempty"`
+	User          User          `json:"user,omitempty"`
+	DeviceStates  []DeviceState `json:"deviceStates,omitempty"`
 }
 
 func (d *Device) BeforeCreate() (err error) {
